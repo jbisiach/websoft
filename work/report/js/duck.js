8c683c6e10b7d6b62fddb821e9c34fb480c0ef92
Jonathon Bisiach <jonathon.bisiach0018@stud.hkr.se>
2020-02-14 14:15:17 +0100

    S04

--- work/report/js/duck.js
+++ work/report/js/duck.js
@@ -1,45 +1,13 @@
-(function(){
+"use strict"
 
-    var duck = document.getElementById("duck")
-    var offsetWidth = duck.offsetWidth
-    var offsetHeight = duck.offsetHeight
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-    var hidden = false;
-
-    setInterval(function(){
-        
-        duck.style.visibility = hidden ? 'visible' : 'hidden'
-        hidden = !hidden
-
-        var newX = Math.floor(Math.random() * (w))
-        var newY = Math.floor(Math.random() * (h))
-
-
-        if(newX >= (w-offsetWidth)){
-            duck.style.left = newX-duck.offsetWidth + "px"
-        } else {
-            duck.style.left = newX + "px"
-        }
-       
-        if(newY >= (h-offsetHeight)){
-            duck.style.top  = newY-duck.offsetHeight+ "px"
-        }else{
-            duck.style.top  = newY+ "px"
-        }
-    },750)
-
-    duck.addEventListener("click", function(){
-        
+function moveElmRand(elm){ 
+    elm.style.position ='absolute'; 
+    elm.style.top = Math.floor(Math.random()*90+5)+'%'; 
+    elm.style.left = Math.floor(Math.random()*90+5)+'%'; 
+   } 
     
-    })
-
-    window.addEventListener("resize", function(){
-         w = window.innerWidth;
-         h = window.innerHeight;
-    })
-
-    console.log(duck.style.width)
-    console.log(duck)
-    console.log("Duck ready")
-})();
+   var duck = document.querySelector('#duck'); 
+    
+   duck.addEventListener('mouseenter', function(e){ moveElmRand(e.target);}); 
+    
+   btn_test.addEventListener('click', function(e){ alert('Fast hands!');});
---
+++ work/s04/stringRange1.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = {
+    "stringRange": stringRange
+};
+
+function stringRange(a, b, sep = ", ") {
+    let res = "";
+    let i = a;
+
+    while (i < b) {
+        res += i + sep;
+        i++;
+    }
+
+    res = res.substring(0, res.length - sep.length);
+    return res;
+}
